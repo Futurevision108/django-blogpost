6e54b03d3577fa220554fb9074050dbea7dd9781
futurevision108 <myfuturevision108@gmail.com>
2025-11-13 15:02:05 +0000

    add:environment variable

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i-3ljnh7&i8124+m---=oy236f-gcs5keio5wwle2%wubfdlzs'
+if os.path.isfile('env.py'):
+    import  env
+
+SECRET_KEY = os.environ.get('SECRET_KEY')  
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
