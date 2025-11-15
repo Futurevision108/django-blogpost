abe93afc346eda57585342e9fc02ab7b350f5658
futurevision108 <myfuturevision108@gmail.com>
2025-11-15 15:44:34 +0000

    add: comment , category and like  model

--- my_blog/admin.py
+++ my_blog/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post ,Comment, Category, Like
 
 # Register your models here.
 admin.site.register(Post)
+admin.site.register(Comment)
+admin.site.register(Category)
+admin.site.register(Like)
--- my_blog/models.py
+++ my_blog/models.py
@@ -17,4 +17,44 @@ class Post(models.Model):
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        ordering = ["-created_on"]
 
+    def __str__(self):
+        return f"{self.title} | written by {self.author}"
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    author = models.CharField(max_length=80)
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.name}"
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, unique=True)
+
+    class Meta:
+        verbose_name_plural = "categories"
+
+    def __str__(self):
+        return self.name
+
+
+class Like(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="likes")
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_on = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        unique_together = ("post", "user")
+
+    def __str__(self):
+        return f"{self.user.username} likes {self.post.title}"
