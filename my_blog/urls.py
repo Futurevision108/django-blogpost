d55829f4666dbf1ae0ffbd2eef1791da60d143e7
futurevision108 <myfuturevision108@gmail.com>
2025-11-16 11:45:25 +0000

    Resolved merge conflict and kept working branch changes

--- my_blog/urls.py
+++ my_blog/urls.py
@@ -2,9 +2,5 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-<<<<<<< HEAD
-    path('', home_page_view),
-=======
-    path('', views.PostList.as_view(), name='home'),
->>>>>>> workingbranch
+ path('', views.PostList.as_view(), name='home'),
 ]
