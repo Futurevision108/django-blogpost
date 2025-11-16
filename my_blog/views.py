8f5374093ecf53b67e47bf373e66104de84e050d
futurevision108 <myfuturevision108@gmail.com>
2025-11-16 11:08:59 +0000

    add: generic post view and static folder with placeholder

--- my_blog/views.py
+++ my_blog/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views import generic
+from .models import Post
+
 
 
 # Create your views here.
-def home_page_view(request):
-    return HttpResponse('<h1>Welcome to My Blog!</h1>')  
-
+class PostList(generic.ListView):
+    queryset = Post.objects.all()
+    template_name = "post_list.html"
